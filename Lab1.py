b1ab5e58e2e89081df547f98d0168d1aba5198a2
Yipu Chen <yipu1@cs.ualberta.ca>
2020-01-08 18:27:50 -0700

    Q8

--- Lab1.py
+++ Lab1.py
@@ -5,3 +5,7 @@ print(requests.__version__)
 google = requests.get("http://google.com/teapot")
 
 print(google)
+
+var = requests.get("https://raw.githubusercontent.com/YipuChen/CMPUT404-Lab1/master/Lab1.py")
+
+print(var.content)
